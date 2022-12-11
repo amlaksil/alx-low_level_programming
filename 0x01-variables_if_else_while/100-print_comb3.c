9ae05503f164951551cda246971e2a97c87ae965
amlaksil <silamlakdesye@gmail.com>
2022-12-11 22:47:03 +0300

    different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,9 @@ int main(void)
 				putchar((i) + '0');
 				putchar((j) + '0');
 				if (i != 8)
-					putchar(',');
-				putchar(' ');
+				{	putchar(',');
+				        putchar(' ');
+				}
 			}
 		}
 	}

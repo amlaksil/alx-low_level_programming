4bc5d807d81054d9983e49a003dff647bb2f6489
amlaksil <silamlakdesye@gmail.com>
2022-12-12 00:31:21 +0300

    different combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,11 +22,8 @@ int main(void)
 						putchar((i) + '0');
 						putchar((j) + '0');
 						putchar((k) + '0');
-						if (i != 7)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}

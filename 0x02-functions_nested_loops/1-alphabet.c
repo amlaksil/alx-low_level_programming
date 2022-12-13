d8c59922a1f4f864e065991fdbbc09789ea69a20
amlaksil <silamlakdesye@gmail.com>
2022-12-13 19:03:11 +0300

    alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <string.h>
 /**
  * main - check the code
  *
@@ -10,14 +8,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-void print_alphabet(void)
-{
-	char lower[] = "abcdefghijklmnopqrstuvwxyz\n";
-
-	_putchar(lower);
-}
-int _putchar(char lower[])
-{
-	return (write(1, lower, strlen(lower)));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,24 @@
+#include <unistd.h>
+#include <string.h>
+/**
+  * _putchar - writes the alphabet in lowercase to stdout
+  * print_alphabet call the function _putchar
+  * Return: On success 1
+  */
 void print_alphabet(void);
 int _putchar(char lower[]);
+
+int _putchar(char lower[])
+{
+	return (write(1, lower, strlen(lower)));
+}
+void print_alphabet(void)
+{
+	char lower[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	_putchar(lower);
+}
+
+
+
 

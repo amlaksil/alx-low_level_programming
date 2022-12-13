90c36e74944708780d39f4e89cf4aed327d82618
amlaksil <silamlakdesye@gmail.com>
2022-12-14 02:55:57 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * @n: The character in ASCII code
+ *
+ * Return: 1, 0, and -1 if it is +ve, zero, and -ve respectively
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+	_putchar('\n');
+}

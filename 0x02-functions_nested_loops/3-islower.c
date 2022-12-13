e38877b9110b71550baafe17048ac7dc81bee4e8
amlaksil <silamlakdesye@gmail.com>
2022-12-14 01:55:19 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,11 @@
 
 /**
   * _islower - Cheks for lowercase character
-  * Return: 1 if it is lower if not 0
+  *
+  * c - is int parameter
+  * Return: 1 if c is lower if not 0
   */
 int _islower(int c)
 {
-	return (_putchar((c != 'C' ? 0 : 1)));
+	return (_putchar((c >= 97 ? 1 : 0)));
 }

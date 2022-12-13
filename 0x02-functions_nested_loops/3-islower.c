e87c798aed2402e411b84167fe8ea544dcf050dd
amlaksil <silamlakdesye@gmail.com>
2022-12-14 01:41:31 +0300

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  * _islower - Cheks for lowercase character
+  * Return: 1 if it is lower if not 0
+  */
+int _islower(int c)
+{
+	return (_putchar((c == c ? 1 : 0)));
+}

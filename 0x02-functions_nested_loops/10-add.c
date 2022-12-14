187591013c3aa87d03acad54127ed562feacbb05
amlaksil <silamlakdesye@gmail.com>
2022-12-14 05:17:06 +0300

    add 2 number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers number
+ *
+ * @a,@b The character in ASCII code
+ *
+ * Return: the sum of a + b
+ */
+int add(int a, int b)
+{
+	int sum = 0;
+	sum = a + b;
+	return (sum);
+}

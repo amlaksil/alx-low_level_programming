4729ce412d3c411551d2cffcdb63f4bbb38195ef
amlaksil <silamlakdesye@gmail.com>
2022-12-14 05:31:12 +0300

    natural no

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ *
+ * @n: natural number
+ */
+void print_to_98(int n)
+{
+	for (n = 1; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}

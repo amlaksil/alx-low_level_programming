9f116954fab3baaedcb55c5ccb4b9a2a08107df2
amlaksil <silamlakdesye@gmail.com>
2022-12-15 00:28:20 +0300

    Update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
+			if (k = 0 && l = 0)
 			_putchar((i) + '0');
+			_putchar((i) + '0')
 			_putchar(':');
+			_putchar((i) + '0');
 			_putchar((j) + '0');
 	   		_putchar('\n');
 		}

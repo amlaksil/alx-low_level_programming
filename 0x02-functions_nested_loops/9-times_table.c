d8c20171d222bb2280304149685834a1f2a6ead5
amlaksil <silamlakdesye@gmail.com>
2022-12-15 02:53:54 +0300

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+  *
+  */
+void times_table(void)
+{
+	int i, k;
+	
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 9; k++)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 18; k += 2)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 27; k += 3)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 36; k += 4)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 45; k += 5)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; K <= 54; k += 6)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 63; k += 7)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 72; k += 8)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (k = 0; k <= 81; k += 9)
+	{
+		_putchar((k) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n')
+
+

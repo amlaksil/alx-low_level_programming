4b39f5b1c519754b905df9abcc9655d83a104147
amlaksil <silamlakdesye@gmail.com>
2022-12-15 01:39:49 +0300

    Update:minute of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -30,22 +30,15 @@ void jack_bauer(void)
 				_putchar((j) + '0');
 				_putchar('\n');
 			}
-			else if (j > 10 && i < 10)
-			{
-				_putchar((l) + '0');
-				_putchar((i) + '0');
-				_putchar(':');
-				_putchar((j) + '0');
-				_putchar('\n');
-			}
 			else
 			{
-				_putchar((i) + '0');
-				_putchar(':');
-				_putchar((l) + '0');
-				_putchar((j) + '0');
-				_putchar('\n');
+				 _putchar((i) + '0');
+				 _putchar(':');
+				 _putchar((l) + '0');
+				 _putchar((j) + '0');
+				 _putchar('\n');
 			}
+
 		}
 	}
 }

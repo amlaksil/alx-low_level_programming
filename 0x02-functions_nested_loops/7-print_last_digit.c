b2914497126fadaded35efa35abd01054517a126
amlaksil <silamlakdesye@gmail.com>
2022-12-14 04:39:29 +0300

    last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int n)
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(48 - l);
+		_putchar(-l + 48);
 		return (-l);
 	}
 	else
 	{
-		_putchar(48 + 1);
+		_putchar(1 + 48);
 		return (l);
 	}
 }

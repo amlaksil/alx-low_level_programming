11fb57468922d813b11af88f8f3f97f602095feb
amlaksil <silamlakdesye@gmail.com>
2022-12-15 01:33:26 +0300

    Update:minute of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,51 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer starting from 00:00 to 23:59
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  *
  * Return:
  */
 void jack_bauer(void)
 {
 	int i, j;
+	int l = 0;
 
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar((i) + '0');
-			_putchar((i) + '0');
-			_putchar(':');
-			_putchar((i) + '0');
-			_putchar((j) + '0');
-	   		_putchar('\n');
+			if (j < 10 && i < 10)
+			{
+				_putchar((l) + '0');
+				_putchar((i) + '0');
+				_putchar(':');
+				_putchar((l) + '0');
+				_putchar((j) + '0');
+				_putchar('\n');
+			}
+			else if (i > 10 && j > 10)
+			{
+				_putchar((i) + '0');
+				_putchar(':');
+				_putchar((j) + '0');
+				_putchar('\n');
+			}
+			else if (j > 10 && i < 10)
+			{
+				_putchar((l) + '0');
+				_putchar((i) + '0');
+				_putchar(':');
+				_putchar((j) + '0');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar((i) + '0');
+				_putchar(':');
+				_putchar((l) + '0');
+				_putchar((j) + '0');
+				_putchar('\n');
+			}
 		}
 	}
 }

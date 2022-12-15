4e5a9ae0e69640e892e18bd7a47aff850e28da43
amlaksil <silamlakdesye@gmail.com>
2022-12-15 04:33:33 +0300

    natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,34 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	int i;
+	i = n;
+
+	if (n >= 98)
 	{
-		while (n >= 98)
+		while (i >= 98)
 		{
-			_putchar((n) + '0');
-			if (n != 98)
+			_putchar((i) + '0');
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n--;
+			i--;
 		}
 	}
-	else
+	else if (n <= 98)
 	{
-		while (n < 98)
+		while (i < 98)
 		{
-			_putchar((n) + '0');
-			if (n != 98)
+			_putchar((i) + '0');
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n++;
+			i++;
 		}
 	}
-	_putchar((98) + '0');
 	_putchar('\n');
 }

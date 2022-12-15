b95f475031390f3efafc7448c87814df5e8707d6
amlaksil <silamlakdesye@gmail.com>
2022-12-15 05:17:43 +0300

    Update:times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,77 +6,32 @@
   */
 void times_table(void)
 {
-	int i, k;
-	
+	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(0 + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	for (k = 0; k <= 9; k++)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 18; k = k + 2)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 27; k += 3)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 36; k += 4)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 45; k += 5)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 54; k += 6)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 63; k += 7)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 72; k += 8)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (k = 0; k <= 81; k += 9)
-	{
-		_putchar((k) + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-
 }

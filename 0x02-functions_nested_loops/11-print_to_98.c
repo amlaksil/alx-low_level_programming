356baeac5a9b66ea61ac644f2734152e21b2133f
amlaksil <silamlakdesye@gmail.com>
2022-12-15 04:20:03 +0300

    natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			i--;
+			n--;
 		}
 	}
 	else
@@ -33,6 +33,6 @@ void print_to_98(int n)
 			n++;
 		}
 	}
-	_putchar(98 + '0');
+	_putchar((98) + '0');
 	_putchar('\n');
 }

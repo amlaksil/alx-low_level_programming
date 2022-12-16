ea9f062635eb15755ca744a8684194a184d45601
amlaksil <silamlakdesye@gmail.com>
2022-12-17 02:19:08 +0300

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k, l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar((j) + '0');
+			k = j / 10;
+			l = j % 10;
+			if (j > 10)
+				_putchar((k) + '0');
+
+			_putchar((l) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }

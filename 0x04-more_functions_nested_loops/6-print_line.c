6c78b407ddb4ca222421717470f68e5fd16e20e5
amlaksil <silamlakdesye@gmail.com>
2022-12-17 02:44:03 +0300

    updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_line - Draws a straight line in the terminal
+  * @n: The character in ASCII code
+  */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	return (0);
+}

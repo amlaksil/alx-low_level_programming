97a46c248bcf4037c770474b4216183f11c6d59c
amlaksil <silamlakdesye@gmail.com>
2022-12-16 22:38:40 +0300

    tasks added

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
   * _isupper - Checks for uppercase character
   *
-  * @c: the character in ascci code
-  * Return: 1 if it is uppercase 0 if not.
+  * @c: The character in ASCII code
+  *
+  * Return: 1 if it is uppercase 0 if not
   */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
@@ -15,5 +18,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-	_putchar('\n');
 }

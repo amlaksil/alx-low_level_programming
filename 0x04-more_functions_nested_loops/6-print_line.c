faea18ee4a1421963dbd70d1c43f6d6aa95c65b2
amlaksil <silamlakdesye@gmail.com>
2022-12-17 03:18:09 +0300

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,17 @@ void print_line(int n)
 {
 	int numlines = 1;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		while (numlines < n)
-		{
-			_putchar('_');
-			numlines++;
-		}
 		_putchar('\n');
 	}
 	else
 	{
+		while (numlines <= n)
+		{
+			_putchar('_');
+			numlines++;
+		}
 		_putchar('\n');
 	}
 }

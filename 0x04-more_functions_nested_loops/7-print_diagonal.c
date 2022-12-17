df9c4653353aeadaa236d34b07edf376d56d75a5
amlaksil <silamlakdesye@gmail.com>
2022-12-17 03:02:20 +0300

    updated

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Draws a diagonal line on the terminal
+  *
+  * @n: The character on ASCII code
+  */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('\');
+			n--;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

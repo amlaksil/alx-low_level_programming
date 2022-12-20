639d208795fd51ecac46f63f6863f989844726e1
amlaksil <silamlakdesye@gmail.com>
2022-12-20 04:39:53 +0300

    reversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_rev - Prints a string in reverse
+  * @s: pointer
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+		if (*(s + i) == '\0')
+			j = i;
+	}
+
+	while (j >= 0)
+	{
+		_putchar(*(s + j));
+		j--;
+	}
+	_putchar('\n');
+}

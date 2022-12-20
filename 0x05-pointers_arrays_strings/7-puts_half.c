fdafb5fde67e721d90fc7359304ded87dc1c11dd
amlaksil <silamlakdesye@gmail.com>
2022-12-20 19:43:21 +0300

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -29,9 +29,9 @@ void puts_half(char *str)
 		else
 		{
 			n = length / 2;
-			while (n < length)
+			while (n <= length)
 			{
-				_putchar(*(str + n));
+				_putchar(*(str + (n + 1)));
 				n++;
 			}
 		}

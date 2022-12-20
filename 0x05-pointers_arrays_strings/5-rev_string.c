2b160589e2f0113fb8af182550a1373f51e74e81
amlaksil <silamlakdesye@gmail.com>
2022-12-20 06:14:52 +0300

    reversed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * rev_string - reverses a string
+  * @s: pointer
+  */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	if (*s == '\0')
+		j = 0;
+	else
+	{
+		while (*(s + i) != '\0')
+		{
+			i++;
+			if (*(s + i) == '\0')
+				j = i;
+		}
+	}
+
+	if (j != 0)
+	{
+		j = j - 1;
+		while (j >= 0)
+		{
+			_putchar(*(s + j));
+			j--;
+		}
+	}
+	_putchar('\n');
+}

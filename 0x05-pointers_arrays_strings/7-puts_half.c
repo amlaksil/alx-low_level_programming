a64ad7a7991a41ae15bf540032128f317cb1dfdd
amlaksil <silamlakdesye@gmail.com>
2022-12-20 16:19:13 +0300

    halved string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts_half - Prints half of a string
+ * @str: pointer
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int length;
+	int n;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+		if (*(str + i) == '\0')
+			length = i;
+	}
+
+	if (length % 2 != 0)
+	{
+		n = (length - 1) / 2;
+		while (n <= length)
+		{
+			_putchar(*(str + n));
+			n++;
+		}
+	}
+	else
+	{
+		n = length / 2;
+		while (n <= length)
+		{
+			_putchar(*(str + n));
+			n++;
+		}
+	}
+	_putchar('\n');
+}

194fc8149727fc237ab0850950e9865747a97511
amlaksil <silamlakdesye@gmail.com>
2022-12-24 16:54:46 +0300

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,24 +14,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int j = 0, i = 0;
 	int length_s;
 
-	if (*src != '\0')
+	while (j < n)
 	{
-		while (*(src + i) != '\0')
-		{
-			i++;
-			if (*(src + i) == '\0')
-				length_s = i;
-		}
-		while (j < n)
-		{
-			*(dest + j) = *(src + j);
-			j++;
-		}
-		while (j < n)
-		{
-			*(dest + j) = '\0';
-			j++;
-		}
+		*(dest + j) = *(src + j);
+		j++;
+	}
+	while (j < n)
+	{
+		*(dest + j) = '\0';
+		j++;
 	}
 	return (dest);
 }

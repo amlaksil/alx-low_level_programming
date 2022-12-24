21842b1bc08745e7ec2bcfca52d3d3d0f4175333
amlaksil <silamlakdesye@mail.com>
2022-12-25 01:36:31 +0300

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,28 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i;
+	int len, j = 0;
 
-	while (op == 0)
+	while (*(s1 + j) != '\0')
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		j++;
+		if (*(s1 + j) == '\0')
+			len = j;
 	}
 
-	return (op);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			return (1);
+
+		if (s1[i] < s2[i])
+			return (-1);
+	}
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (i == len)
+			return (0);
+	}
+	return (0);
 }

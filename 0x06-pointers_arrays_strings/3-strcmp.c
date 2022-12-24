3dce3d6e92ce9d0f6b3fe543a0eb867fd0a3e83c
amlaksil <silamlakdesye@mail.com>
2022-12-25 01:20:35 +0300

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,30 +7,17 @@
  * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int len, j = 0;
+	int i = 0, op = 0;
 
-	while (*(s1 + j) != '\0')
+	while (op == 0)
 	{
-		j++;
-		if (*(s1 + j) == '\0')
-			len = j;
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] > s2[i])
-			return (1);
-
-		if (s1[i] < s2[i])
-			return (-1);
-	}
-	for (i = 0; s1[i] == s2[i]; i++)
-	{
-		if (i == len)
-			return (0);
-	}
+	return (op);
 }

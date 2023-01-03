3e45aa0d7d0b34671981e3a6a39c320550b5039a
amlaksil <silamlakdesye@mail.com>
2023-01-03 10:05:12 +0300

    Matched initial segment returned

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,21 +2,30 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string pointer
- * @accept: consists the initial segment of s
+ * @s: a string to be matched
+ * @accept: accepted string
  *
  * Return: number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, len;
+	unsigned int i, j, len = 0, match;
 
-	while (*(accept + i) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i++;
-		if (*(accept + i) == '\0')
-			len = i;
+		match = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				match = 1;
+				break;
+			}
+		}
+		if (match == 0)
+			break;
 	}
 
-	return ((len + 1));
+	return (len);
 }

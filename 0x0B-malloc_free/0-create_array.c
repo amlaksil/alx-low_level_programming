2a45326addf53a0eb115ad37f6bd0338834f9970
amlaksil <silamlakdesye@mail.com>
2023-01-10 20:45:52 +0300

    Add a program that return pointer which containes a copy of string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,7 @@ char *create_array(unsigned int size, char c)
 	ptr = malloc(sizeof(*ptr) * size);
 	if (size == 0)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			*(ptr + i) = c;
-		}
-		return (ptr);
-	}
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer which contains a copy of the string given
+ * @str: a pointer which contains string
+ *
+ * Return: NULL if str = NULL or a pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	ptr = (char *) malloc(strlen(str));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}

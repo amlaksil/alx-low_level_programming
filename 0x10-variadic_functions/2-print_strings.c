80d81c1161d906a92a7dcf81e428405ffd05eaba
amlaksil <silamlakdesye@mail.com>
2023-01-19 14:54:42 +0300

    Add program that print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints string
+ * @separator: the string to be printed between strings
+ * @n: the number of strings passed to the function
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}

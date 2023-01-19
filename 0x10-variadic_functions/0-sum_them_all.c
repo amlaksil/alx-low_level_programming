cbdd559c991326c86bd9dad79ba177506ff39934
amlaksil <silamlakdesye@mail.com>
2023-01-19 13:15:16 +0300

    Add a program that sum all of its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all of its parameters
+ * @n: unsigned integer parameter
+ *
+ * Return: the total sum of all its parameters
+ * if n is zero, return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+
+	return (sum);
+}

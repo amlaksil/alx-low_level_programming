c175aeb806c393d45badce0fbe6ebd444246d590
amlaksil <silamlakdesye@gmail.com>
2023-01-31 22:53:03 +0300

    Fix bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 	ptr = *head;
-	while (ptr != NULL)
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (ptr);
+	}
+	while (ptr != NULL && idx != 0)
 	{
 		if (insertion_index == (idx - 1))
 		{

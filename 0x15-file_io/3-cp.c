56e0e9981e77376cf20bc6c930f2ed164d92fce0
amlaksil <silamlakdesye@gmail.com>
2023-02-11 05:44:45 +0300

    Fix bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,74 +1,44 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-
 /**
- * error_msg - check for error message
- * @fd_from: source file
- * @fd_to: destination
- * @av: argument vector
+ * main - copies the content of a file to another file.
  *
- * Return: nothing
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return: Always 0.
  */
-
-void error_msg(int fd_from, int fd_to, char *av[])
+int main(int argc, char *argv[])
 {
-	if (fd_from == -1)
+	int fd1, fd2, chars_read, chars_written, file_closed, file2_closed;
+	char *buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(argv[1], O_RDONLY, 0);
+	if (fd1 == -1)
 	{
-		dprintf(2, "Error: can't read from %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (fd_to == -1)
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((chars_read = read(fd1, buf, 1024)) > 0)
 	{
-		dprintf(2, "Error: can't write to %s\n", av[2]);
-		exit(99);
+		chars_written = write(fd2, buf, chars_read);
+		if (chars_written != chars_read)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-}
-
-/**
- * main - copies the content of a file to another file
- * @ac: argument count
- * @av: agrumnt vector
- *
- * Return: 0 (on success)
- */
-
-int main(int ac, char **av)
-{
-	int fd_from, fd_to, wr, n = 1024;
-	int c_err_f, c_err_t;
-	char buf[1024];
-
-	if (ac != 3)
+	if (chars_read == -1)
 	{
-		dprintf(2, "Usage: cp file_from file_to \n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	fd_from = open(av[1], O_RDONLY);
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	error_msg(fd_from, fd_to, av);
-
-	while (n == 1024)
-	{
-		n = read(fd_from, buf, 1024);
-		if (n == -1)
-			error_msg(-1, 0, av);
-		wr = write(fd_to, buf, 1024);
-		if (wr == -1)
-			error_msg(0, -1, av);
-	}
-	c_err_f = close(fd_from);
-	if (c_err_f == -1)
-	{
-		dprintf(2, "Error: can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	c_err_t = close(fd_to);
-	if (c_err_t == -1)
-	{
-		dprintf(2, "Error: can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	file_closed = close(fd1);
+	if (file_closed != 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	file2_closed = close(fd2);
+	if (file2_closed != 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (0);
 }

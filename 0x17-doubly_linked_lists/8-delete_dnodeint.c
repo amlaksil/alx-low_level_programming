d1584fb0fb3633fc379b8077428f16c270f45ad6
amlaksil <silamlakdesye@gmail.com>
2023-03-17 16:45:04 +0300

    Delete node at given index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,35 +3,70 @@
 /**
  * delete_dnodeint_at_index - deletes the node at the given index
  * @head: pointer to the first element in the list
- * @index: index given
+ * @index: a position to be given, it starts at 0
  *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *ptr = *head;
-	dlistint_t *temp;
-	unsigned int num = 0;
+	dlistint_t *ptr = *head, *current = *head, *temp, *dtemp;
+	unsigned int position = 0, last_node = 0;
 
+	if (ptr == NULL)
+		return (-1);
+	last_node = dlistint_len(current);
 	if (index == 0)
 	{
-		ptr = ptr->next;
-		ptr->prev = NULL;
+		temp = ptr->next, dtemp = ptr, ptr->prev = NULL;
+		free(dtemp), *head = temp;
 		return (1);
 	}
-	while (ptr != NULL)
+	else if (last_node == index)
 	{
-		if (num == index - 1)
+		while (ptr != NULL)
 		{
-			ptr->next = ptr->next->next;
-			temp = ptr;
-			ptr = ptr->next->next;
-			free(ptr->prev->prev);
-			ptr->prev = temp;
-			return (1);
+			position++, ptr = ptr->next;
+			if (position == last_node - 1)
+			{
+				dtemp = ptr->next, ptr->next = NULL;
+				free(dtemp);
+				return (1);
+			}
 		}
-		num++;
-		ptr = ptr->next;
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			if (position == index - 1)
+			{
+				temp = ptr, dtemp = ptr->next;
+				ptr->next = ptr->next->next, ptr = ptr->next;
+				ptr->prev = temp, free(dtemp);
+				return (1);
+			}
+			position++, ptr = ptr->next;
+		}
 	}
 	return (-1);
+}
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: points to the start of a list
+ *
+ * Return: the number of node
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *head = h;
+	size_t num_node = 0;
+
+	while (head != NULL)
+	{
+		num_node++;
+		head = head->next;
+	}
+	return (num_node);
 }

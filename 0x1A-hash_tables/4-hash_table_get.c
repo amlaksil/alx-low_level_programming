647e5171038da195271bbad04e430d31194d27c7
amlaksil <silamlakdesye@gmail.com>
2023-04-15 18:53:07 +0300

    Consider collision while retrieve value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdio.h>
+#include <string.h>
 /**
   * hash_table_get - find the value associated with a key
   * @ht: hash table which may contains the value we look for
@@ -11,11 +12,37 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, size;
+	hash_node_t *ptr;
+
+	if (ht == NULL)
+		return (NULL);
 
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
 	if (ht->array[index] != NULL)
-		return (ht->array[index]->value);
+	{
+		if (ht->array[index]->next == NULL)
+		{
+			return (ht->array[index]->value);
+		}
+		else
+		{
+			ptr = ht->array[index];
+			while (ptr != NULL)
+			{
+				/**
+				 * Collision occure so we have to traverse through
+				 * the list to get the value associated with a given key
+				 */
+				if (strcmp(ptr->key, key) == 0)
+				{
+					printf("got it\n");
+					return (ptr->value);
+				}
+				ptr = ptr->next;
+			}
+		}
+	}
 
 	return (NULL);
 }
